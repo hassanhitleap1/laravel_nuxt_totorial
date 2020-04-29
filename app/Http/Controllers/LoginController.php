e6d84863c7daa787380948f925c04ac97f6709fb
hassan kiwan <hassanhitleap1@gmail.com>
2020-04-29 18:03:44 +0300

    103

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Resources\User as UserResources;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    /**
+     * @param UserLoginRequest $request
+     * @return UserResources|\Illuminate\Http\JsonResponse
+     */
     public function index(UserLoginRequest $request)
     {
         if(!$token=auth()->attempt($request->only('email','password'))){
-            return abort(401);
+            return response()->json([
+                'error'=>[
+                    'error must be shure user name or email'
+                ]
+            ]);
         }
 
         return  (new UserResources($request->user()))->additional([
@@ -18,6 +27,14 @@ class LoginController extends Controller
                 'token'=>$token,
             ],
         ]);
-      
+
+    }
+
+    public function user(Request $request){
+        return new UserResources($request->user());
+    }
+
+    public  function  login(){
+        auth()->logout();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,5 @@
 <?php
+Route::post('/login','LoginController@index');
+Route::post('/register','RegistrarController@index');
+Route::get('/user','LoginController@user');
+Route::post('/user','LoginController@user');
